bb703645abb7d6d645afff99236d6865a53645cc
zhuko <zhukov_ai@outlook.com>
2024-11-21 17:42:51 +0300

    Я не умру сказал дед, и умер

--- view.py
+++ view.py
@@ -1,13 +1,11 @@
-import pygame,model,random
-
-
+import pygame, model, random
 
 pygame.init()
 font = pygame.font.SysFont("arial", 20, True)
 
-
 display = pygame.display.set_mode([750, 750])
 pygame.display.set_caption("Шариковый беспредел")
+
 
 def risovanie():
     global y_nadpisi
@@ -15,13 +13,9 @@ def risovanie():
     display.fill([0, 0, 0])
 
     for infa_sharik in model.all_sharik:
-        pygame.draw.circle(display,infa_sharik["color"],infa_sharik["coord"],infa_sharik["radius"])
-
-
-
-        infa_sharik["coord"]+=1
-
-
+        pygame.draw.circle(display, infa_sharik["color"], infa_sharik["coord"], infa_sharik["radius"])
+        #TODO: Строка с увеличением скорости шарика находиться в форе, а фор перекладывает щарики в другую переменную, так что чем больше шариков тем больше раз увеличивается скорость шарика на 0.1
+        model.all_sharik[0]["coord"][0] += 0.1
 
     if model.visible_settings == True:
         all_nadpisi = [
@@ -31,17 +25,13 @@ def risovanie():
             "Del - Удалить все шарики",
             "Шариков в секунду:" + str(model.sharik_per_second),
             "Всего шариков:" + str(len(model.all_sharik))
-
         ]
 
         for all_nadpisi_zapis in all_nadpisi:
-            all_nadpisi_render = font.render(all_nadpisi_zapis,True,[255,255,255])
+            all_nadpisi_render = font.render(all_nadpisi_zapis, True, [255, 255, 255])
             all_nadpisi_render.convert_alpha()
             all_nadpisi_render.set_alpha(model.alpha)
-            display.blit(all_nadpisi_render,[0,y_nadpisi])
+            display.blit(all_nadpisi_render, [0, y_nadpisi])
             y_nadpisi += 25
-
-
-
 
     pygame.display.flip()
